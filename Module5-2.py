95fb88f07509953f8b6a15feedeef6f09f78c5d7
2010030372 <95161366+2010030372@users.noreply.github.com>
2022-03-18 09:06:35 +0530

    Create Module5-2.py

---
+++ Module5-2.py
@@ -0,0 +1,26 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'Prasu_26',
+    database = 'BOOKS'
+)
+cursor = mydb.cursor()
+while True:
+    x=input("want to enter data yes/no: ")
+    if x=='yes':
+        title=input("Enter book name: ")
+        author=input("Enter author name: ")
+        price=input("Enter price of book: ")
+        sql = "INSERT INTO NAMES(title,author, price) VALUES(%s, %s, %s)"
+        val = (title, author, price)
+        try:
+            cursor.execute(sql, val)
+            mydb.commit()
+        except:
+            mydb.rollback()
+        print(cursor.rowcount, "record inserted!")
+        mydb.close()
+
+    else:
+        break
