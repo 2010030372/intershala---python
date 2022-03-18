a5b0157f785f4b92235b0790f5c89cb21d64c5f2
2010030372 <95161366+2010030372@users.noreply.github.com>
2022-03-18 09:09:22 +0530

    Update Module5-1.py

--- Module5-1.py
+++ Module5-1.py
@@ -2,7 +2,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    password='Prasu_26'
+    password='****'
 )
 cursor = mydb.cursor()
 cursor.execute("CREATE DATABASE BOOKS")
